047bb92118252703213d96ae26d78ca2914fa690
Bethina Dileep <56349269+Bethinadileep@users.noreply.github.com>
2022-02-27 10:34:37 +0530

    Update BFS.java

--- BFS.java
+++ BFS.java
@@ -91,5 +91,42 @@ class Solution {
         return bfs; 
         
         // Code here
+        /*
+        class Solution {
+    // Function to return Breadth First Traversal of given graph.
+    public ArrayList<Integer> bfsOfGraph(int V, ArrayList<ArrayList<Integer>> adj) {
+        // Code here
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean vis[] = new boolean[V];
+        for(int i = 0; i < V; i++) {
+            if(vis[i] == false){
+                Queue<Integer> q = new LinkedList<>();
+                q.add(i);
+                vis[i] = true;
+                
+                while(!q.isEmpty()){
+                    Integer node = q.poll();
+                    bfs.add(node);
+                    
+                    for(Integer it : adj.get(node)) {
+                        if(vis[it] == false){
+                            vis[it] = true;
+                            q.add(it);
+                        }
+                    }
+                }
+            }
+        }
+        
+        return bfs;
     }
 }
+        
+        
+        
+        */
+        
+    }
+}
+
+
